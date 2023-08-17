d2f99d7024684ef91136ab29465160092b52ffc0
Vlad Astahov <avl@burno.tech>
2023-08-18 01:23:05 +0300

    minor

--- src/widgets/quiz/ui/Result/Result.tsx
+++ src/widgets/quiz/ui/Result/Result.tsx
@@ -5,7 +5,7 @@ import styles from './Result.module.scss'
 
 export const Result = () => {
     return <div className={styles.root}>
-        <LazyLoadImage src="/images/decoration.svg" className={styles.image} />
+        <LazyLoadImage src="/quizJS/images/decoration.svg" className={styles.image} />
         <p className={styles.text}>Спасибо за участие в игре! Вы набрали <span>100</span> баллов и заняли <span>10</span> место. Ждем вас в следующей игре, сообщим о ней по email!</p>
     </div>
 }
